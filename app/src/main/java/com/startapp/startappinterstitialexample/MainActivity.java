6f39ce7dc38b0b1c4752b00c51c92bc8d1343496
Indodeks <44467145+Indodeks@users.noreply.github.com>
2018-10-28 00:48:43 +0700

    Update MainActivity.java

--- app/src/main/java/com/startapp/startappinterstitialexample/MainActivity.java
+++ app/src/main/java/com/startapp/startappinterstitialexample/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		StartAppSDK.init(this, "ApplicationID", true); //TODO: Replace with your Application ID
+		StartAppSDK.init(this, "209103345", true); //TODO: Replace with your Application ID
 		
 		setContentView(R.layout.activity_main);
 		
